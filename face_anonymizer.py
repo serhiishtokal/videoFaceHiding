b31aa68cb2d998ade811115de6f9eda6dc5bd2b8
serhiishtokal <serhiishtokal@gmail.com>
2021-04-14 16:48:06 +0200

    added dnn face detector with caffe model

--- face_anonymizer.py
+++ face_anonymizer.py
@@ -5,7 +5,7 @@ from face_hiders.image_anonymizer import anonymize_face, AnonymizeMethod
 import time
 
 video_file_path = 'd:\STUDY Univer\PolitechnikaLubelska\DYPLOM\Datasets\TikTok\download.mp4'
-output_file_path = join(dirname(video_file_path), "1.mp4")
+output_file_path = join(dirname(video_file_path), "DNN Caffe.mp4")
 output_directory_path = join(dirname(video_file_path), "anon")
 
 
@@ -30,6 +30,8 @@ def anonymize_stream_opencv(detection_method=DetectionMethod.CASCADE, anonymize_
         cv2.imshow('Video', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+    video_capture.release()
+    cv2.destroyAllWindows()
 
 
 def anonymize_video_opencv(input_video_path, output_video_path, detection_method=DetectionMethod.CASCADE,
@@ -73,20 +75,24 @@ def anonymize_video_to_images_opencv(input_video_path, output_imeges_directory,
 
     while video_capture.isOpened():
         ret, frame = video_capture.read()
-        if ret:
-            anonymized_frame = _get_anonymized_frame(frame, detection_method, anonymize_method)
-            output_path = join(output_imeges_directory, "%s.jpeg" % frame_count)
-            # cv2.imwrite(output_path, anonymized_frame)
-            frame_count += 1
-            print('[INFO] frames processed: ', frame_count)
-        else:
+        if not ret:
             break
+        anonymized_frame = _get_anonymized_frame(frame, detection_method, anonymize_method)
+        output_path = join(output_imeges_directory, "%s.jpg" % frame_count)
+        cv2.imwrite(output_path, anonymized_frame)
+        frame_count += 1
+        print('[INFO] frames processed: ', frame_count)
+    video_capture.release()
+    cv2.destroyAllWindows()
 
-
-# hide_face_on_video(video_file_path, output_file_path)
+# time.sleep(20)
 start = time.time()
-# anonymize_video_to_images_opencv(video_file_path, output_directory_path)
-anonymize_video_opencv(video_file_path, output_file_path)
+# CASCADE
+# anonymize_stream_opencv(DetectionMethod.DNN_CAFFE)
+# anonymize_video_to_images_opencv(video_file_path, output_directory_path,DetectionMethod.DNN_CAFFE)
+anonymize_video_opencv(video_file_path, output_file_path, DetectionMethod.DNN_CAFFE)
+
+
 end = time.time()
 
 print('start time: ', start, 'end time: ', end, ' elapsed: ', end - start)
--- face_detectors/face_detector.py
+++ face_detectors/face_detector.py
@@ -1,18 +1,19 @@
 import sys
 
 from face_detectors.cascade_face_detector.cascade_face_detector import detect_faces_cascade
+from face_detectors.dnn_caffe_face_detector.dnn_face_detector import detect_faces_dnn_caffe
 
 
 class DetectionMethod:
     CASCADE = 1
-    HOG = 2
+    DNN_CAFFE = 2
     DNN = 3
 
 
 def detect_faces(image, method=DetectionMethod.CASCADE):
     switcher = {
         DetectionMethod.CASCADE: detect_faces_cascade,
-        DetectionMethod.HOG: _detect_faces_hog,
+        DetectionMethod.DNN_CAFFE: detect_faces_dnn_caffe,
         DetectionMethod.DNN: _detect_faces_dnn
     }
     faces = switcher.get(method, lambda: __invalid_method_exception())(image)
